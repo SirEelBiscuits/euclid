c3b573df7574ff18d42ba2f1b9f708e22159026f
Jameson Thatcher <sireel@bluescreenofdoom.com>
2015-09-19 12:47:47 +0100

    running with SDL.dll

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,17 +1,5 @@
-#include <SDL.h>
-
-#include <iostream>
 #include <cstdlib>
 
-#include <lua.hpp>
-
 int main(int argc, char* argv[]) {
-	SDL_Event e;
-	SDL_PollEvent(&e);
-
-
-	auto test = luaL_newstate();
-
-	std::cout << "hello world" << test << std::endl;
 	return EXIT_SUCCESS;
 }
