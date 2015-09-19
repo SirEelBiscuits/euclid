1b53cf93610815f33870cb35f15a91a51cdacdd2
Jameson Thatcher <sireel@bluescreenofdoom.com>
2015-09-19 12:31:19 +0100

    SDL2 builds

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,9 +1,14 @@
+#include <SDL.h>
+
 #include <iostream>
 #include <cstdlib>
 
 #include <lua.hpp>
 
 int main(int argc, char* argv[]) {
+	SDL_Event e;
+	SDL_PollEvent(&e);
+
 
 	auto test = luaL_newstate();
 
