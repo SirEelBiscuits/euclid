61cbfde8acfce5b12e92cde62157989430a6ad04
Jameson Thatcher <sireel@bluescreenofdoom.com>
2016-03-11 15:39:58 +0000

    file reloading now only checks one file per frame. if you have lots of files this might be a problem I guess?

--- engine/src/platform/SDLAndWindows/Files.cpp
+++ engine/src/platform/SDLAndWindows/Files.cpp
@@ -8,6 +8,7 @@ POST_STD_LIB
 namespace System { namespace Events {
 
 static std::map<std::string, std::pair<FILETIME, handler_type>> filenames;
+static decltype(begin(filenames)) iter;
 
 FILETIME GetLastWriteTime(char const *filename) {
 	//TODO error handling, aliased filenames
@@ -19,6 +20,8 @@ FILETIME GetLastWriteTime(char const *filename) {
 bool RegisterFileToWatch(char const *filename, handler_type handler) {
 	auto writetime = GetLastWriteTime(filename);
 	filenames[filename] = std::pair<FILETIME,handler_type>(writetime, handler);
+
+	iter = begin(filenames);
 
 	return true;
 }
@@ -33,12 +36,14 @@ bool cmpFileTime(FILETIME a, FILETIME b) {
 }
 
 void TickFileWatchers() {
-	for(auto & kvp : filenames) {
-		auto writetime = GetLastWriteTime(kvp.first.c_str());
-		if(cmpFileTime(kvp.second.first, writetime)) {
-			kvp.second.first = writetime;
-			kvp.second.second(kvp.first.c_str());
-		}
+	iter++;
+	if(iter == end(filenames))
+		iter = begin(filenames);
+	auto & kvp = *iter;
+	auto writetime = GetLastWriteTime(kvp.first.c_str());
+	if(cmpFileTime(kvp.second.first, writetime)) {
+		kvp.second.first = writetime;
+		kvp.second.second(kvp.first.c_str());
 	}
 }
 
