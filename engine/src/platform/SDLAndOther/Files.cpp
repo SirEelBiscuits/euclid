e72a5892aaee1adc3489e061a822c2c59260c715
Jameson Thatcher <jameson@bluescreenofdoom.com>
2016-04-12 12:54:16 +0000

    Fully merged and working

--- engine/src/platform/SDLAndOther/Files.cpp
+++ engine/src/platform/SDLAndOther/Files.cpp
@@ -9,7 +9,6 @@ namespace System { namespace Events {
 	static std::set<std::string> fileList{};
 
 	bool RegisterFileToWatch(char const *filename, handler_type handler) {
-		printf("\n*loading %s\n", filename);
 		fileList.insert(filename);
 		return true;
 	}
@@ -18,7 +17,6 @@ namespace System { namespace Events {
 
 	bool IsFileBeingWatched(char const * filename) {
 		auto ret = fileList.find(filename) != fileList.end();
-		printf("check %s, returning: %s\n", filename, ret ? "true" : "false");
 		return ret;
 	}
 
